a253ec5e0eeea610c09a895299b6186fdc5831fa
xam74er <xam74er1@gmail.com>
2019-02-04 14:52:59 +0100

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((640, 480))
+
+#Chargement et collage du fond
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond, (0,0))
+
+#Rafraîchissement de l'écran
+pygame.display.flip()
+
+#BOUCLE INFINIE
+continuer = 1
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = 0
+            pygame.quit()
+    continuer = int(input())
