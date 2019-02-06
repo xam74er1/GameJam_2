6051d2ff40afb7fc0d5345753d586faefa1d0615
Laowaii <arthur425000@outlook.fr>
2019-02-06 17:35:42 +0100

    Merge branch 'master' of https://github.com/xam74er1/GameJam_2

--- test_credit.py
+++ test_credit.py
@@ -1,15 +1,32 @@
-import pygame, sys
+import pygame
 
-# ecrire dans un fichier
-reponse = input("Votre Nom :") # lit ce que tape le clavier dans le terminal
-fichier = open("data.txt", "w") # w ecrase l'ancien fichier, a ecrie a la fin du fichier
-fichier.write(reponse)
-fichier.close()
+def triNoms():
+    fichier = open("noms.txt", "r")
+
+    NumberOfLine = 0
+    for line in fichier:
+        NumberOfLine += 1
+
+    fichier.close()
+
+    fichier = open("noms.txt", "r")
+    tableau = []
+
+    i = 0
+    while i < NumberOfLine:
+        tableau.append(fichier.readline().split("|"))
+        i += 1
+    fichier.close()
+
+    tableau.sort()
+    return tableau
 
 
-# lire dans un fichier
-fichier = open("data.txt", "r")
-print(fichier.read())
-fichier.close()
+def ecrireNoms(tab):
+    i = 0
+    while i < len(tab):
+        print(tab[i][0] + " dans le rôle du " + tab[i][1])
+        i += 1
 
-
+tableau = triNoms()
+ecrireNoms(tableau)
