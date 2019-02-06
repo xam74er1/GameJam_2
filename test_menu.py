c9857d6678cae151e9309db536a0e0982986a5bd
Grillender <s.armand38@laposte.net>
2019-02-06 13:07:35 +0100

    highscore qui marche (plus)

--- test_menu.py
+++ test_menu.py
@@ -11,24 +11,16 @@ def updateimage(frame,array):
 
 # Changement page
 def loadMenu():
-    global current_page
-    current_page = "menu"
-    print("menu")
+    return "menu"
 
 def loadJouer():
-    global current_page
-    current_page = "jouer"
-    print("jouer")
+    return "jouer"
 
 def loadHighscore():
-    global current_page
-    current_page = "highscore"
-    print("highscore")
+    return "hoghscore"
 
 def loadCredit():
-    global current_page
-    current_page = "credit"
-    print("credit")
+    return "credit"
 
 def quitGame():
     pygame.display.quit()
@@ -43,101 +35,114 @@ current_page = "menu"
 # Ouverture de la fenêtre Pygame
 fenetre = pygame.display.set_mode((750, 750))
 
+partie = 1
 
-if current_page == "menu":
-    # Chargement et collage du fond
-    #fond = pygame.image.load("images/background.jpg").convert()
-    #fenetre.blit(fond, (0, 0))
-    titre = pygame.transform.scale(pygame.image.load("Rectangle_bleu_de_merde.png"), (350, 150))
-
-    #list de tout les truc as update
-    arrayUpdate = []
-
-    #list de tout les truc qui attendre un clique , equivant as a event listner pour les boutton
-    arrayClick = []
-
-    #Creation du bouton
-
-    jouer = Bouton("Rectangle_bleu_de_merde.png",275,250,200,50)
-    fenetre.blit(jouer.image,jouer.rect)
-
-
-    highscore = Bouton("Rectangle_bleu_de_merde.png",275,350,200,50)
-    fenetre.blit(highscore.image,highscore.rect)
-
-    credit = Bouton("Rectangle_bleu_de_merde.png",275,450,200,50)
-    fenetre.blit(credit.image,credit.rect)
-
-    quitter = Bouton("Rectangle_bleu_de_merde.png",275,550,200,50)
-    fenetre.blit(quitter.image,quitter.rect)
-
-    #Afection de la fonction as mettre lorsque lon fait laction
-
-    jouer.setButtonAction(loadJouer)
-    highscore.setButtonAction(loadHighscore)
-    credit.setButtonAction(loadCredit)
-    quitter.setButtonAction(quitGame)
-
-    # Rafraîchissement de l'écran
-    pygame.display.flip()
-    # BOUCLE INFINIE
-    continuer = 1
-
-    #Ajout de tout les truc as update
-    #arrayUpdate.append((fond,(0,0)))
-    arrayUpdate.append((jouer.image,jouer.rect))
-    arrayUpdate.append((highscore.image,highscore.rect))
-    arrayUpdate.append((credit.image,credit.rect))
-    arrayUpdate.append((quitter.image,quitter.rect))
-
-    #Ajout de tout les truc qui attende un event
-    arrayClick.append(jouer)
-    arrayClick.append(highscore)
-    arrayClick.append(credit)
-    arrayClick.append(quitter)
-
-
-    while continuer:
-
-        for event in pygame.event.get():  # Attente des événements
-            if event.type == QUIT:
-                continuer = 0
-
-            if event.type == MOUSEBUTTONDOWN:
-
-                if event.button == 1:  # Si clic gauche
-
-                    for bt in arrayClick:
-
-                        val = bt.isInZone(event.pos[0],event.pos[1])
-
-                        if bt.isInZone(event.pos[0],event.pos[1]) :
-                            bt.action()
-
-        # Re-collage
+while partie:
+    print("test")
+    if current_page == "menu":
+        print("page "+current_page)
+        # Chargement et collage du fond
+        #fond = pygame.image.load("images/background.jpg").convert()
         #fenetre.blit(fond, (0, 0))
-        fenetre.blit(titre, (200, 50))
-        updateimage(fenetre,arrayUpdate)
-        #fenetre.blit(perso, position_perso)
-        # Rafraichissement
+        titre = pygame.transform.scale(pygame.image.load("Rectangle_bleu_de_merde.png"), (350, 150))
+
+        #list de tout les truc as update
+        arrayUpdate = []
+
+        #list de tout les truc qui attendre un clique , equivant as a event listner pour les boutton
+        arrayClick = []
+
+        #Creation du bouton
+
+        jouer = Bouton("Rectangle_bleu_de_merde.png",275,250,200,50)
+        fenetre.blit(jouer.image,jouer.rect)
+
+
+        highscore = Bouton("Rectangle_bleu_de_merde.png",275,350,200,50)
+        fenetre.blit(highscore.image,highscore.rect)
+
+        credit = Bouton("Rectangle_bleu_de_merde.png",275,450,200,50)
+        fenetre.blit(credit.image,credit.rect)
+
+        quitter = Bouton("Rectangle_bleu_de_merde.png",275,550,200,50)
+        fenetre.blit(quitter.image,quitter.rect)
+
+        #Afection de la fonction as mettre lorsque lon fait laction
+
+        jouer.setButtonAction(loadJouer)
+        highscore.setButtonAction(loadHighscore)
+        credit.setButtonAction(loadCredit)
+        quitter.setButtonAction(quitGame)
+
+        # Rafraîchissement de l'écran
         pygame.display.flip()
-        pygame.key.set_repeat(40,30)
+        # BOUCLE INFINIE
+        continuer = 1
+
+        #Ajout de tout les truc as update
+        #arrayUpdate.append((fond,(0,0)))
+        arrayUpdate.append((jouer.image,jouer.rect))
+        arrayUpdate.append((highscore.image,highscore.rect))
+        arrayUpdate.append((credit.image,credit.rect))
+        arrayUpdate.append((quitter.image,quitter.rect))
+
+        #Ajout de tout les truc qui attende un event
+        arrayClick.append(jouer)
+        arrayClick.append(highscore)
+        arrayClick.append(credit)
+        arrayClick.append(quitter)
+
+
+        while continuer:
+
+            for event in pygame.event.get():  # Attente des événements
+                if event.type == QUIT:
+                    continuer = 0
+                    quit()
+
+                if event.type == MOUSEBUTTONDOWN:
+
+                    if event.button == 1:  # Si clic gauche
+
+                        for bt in arrayClick:
+
+                            val = bt.isInZone(event.pos[0],event.pos[1])
+
+                            if bt.isInZone(event.pos[0],event.pos[1]) :
+                                current_page = bt.action()
+                                print(current_page)
+                                break
 
 
 
 
-elif current_page == "jouer":
-    titre = pygame.transform.scale(pygame.image.load("Rectangle_bleu_de_merde.png"), (350, 150))
-    fenetre.blit(titre, (500, 500))
-    pygame.display.flip()
+            # Re-collage
+            #fenetre.blit(fond, (0, 0))
+            fenetre.blit(titre, (200, 50))
+            updateimage(fenetre,arrayUpdate)
+            #fenetre.blit(perso, position_perso)
+            # Rafraichissement
+            pygame.display.flip()
+            pygame.key.set_repeat(40,30)
 
 
-elif current_page == "highscore":
-    fond = pygame.image.load("images/background.jpg").convert()
-    fenetre.blit(fond, (0, 0))
+
+
+    elif current_page == "jouer":
+        print("page "+current_page)
+        titre = pygame.transform.scale(pygame.image.load("Rectangle_bleu_de_merde.png"), (350, 150))
+        fenetre.blit(titre, (500, 500))
+        pygame.display.flip()
+
+
+    elif current_page == "highscore":
+        print("page "+current_page)
+        fond = pygame.image.load("images/background.jpg").convert()
+        fenetre.blit(fond, (0, 0))
 
 
 
-elif current_page == "credit":
-    pygame.display.flip()
+    elif current_page == "credit":
+        print("page "+current_page)
+        pygame.display.flip()
 
--- test_tri_score.py
+++ test_tri_score.py
@@ -1,6 +1,6 @@
 import pygame
 
-val = 0
+val = 6
 
 def triScore():
     fichier = open("test-score.txt", "r")
@@ -24,7 +24,7 @@ if int(tab[9][0]) > val:
     while i < len(tab):
         if i < 10:
             print(str(i + 1) + " - " + tab[i][0] + "  -  " + tab[i][1])
-        elif int(tab[i][0]) <= val and nom == False:
+        elif val <= int(tab[i][0]) and nom == False:
             print("")
             print(str(i + 1) + " - " + tab[i][0] + "  -  " + "taper votre nom ...")
             nom = True
