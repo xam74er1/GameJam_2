0a82af30270a857f7e3e6a9572cd1cff61236357
Grillender <s.armand38@laposte.net>
2019-02-06 15:25:13 +0100

    Merge branch 'master' of https://github.com/xam74er1/GameJam_2
    # Conflicts:
    #	test_tri_score.py

--- test_tri_score.py
+++ test_tri_score.py
@@ -1,7 +1,5 @@
 import pygame
 
-val = 2
-
 def triScore():
     fichier = open("test-score.txt", "r")
     tableau = []
@@ -13,41 +11,43 @@ def triScore():
     i = 0
     while i < NumberOfLine:
         tableau.append(fichier.readline().split('|'))
-        i+=1
+        print(fichier.readline().split('|'))
+        #print(tableau.append(fichier.readline().split('|')))
+        i += 1
     fichier.close()
 
     tableau.sort(reverse=True)
     return tableau
 
-
-tab = triScore()
-
-i = 0
-nom = False
-if int(tab[9][0]) > val:
-    while i < len(tab):
-        if i < 10:
-            print(str(i + 1) + " - " + tab[i][0] + "  -  " + tab[i][1])
-            i += 1
-        elif val <= int(tab[i][0]) and nom == False:
-            print("")
-            print(str(i + 1) + " - " + str(val) + "  -  " + "taper votre nom ...")
-            nom = True
-            i += 1
-        else:
-            i += 1
-            print(i)
-else:
-    while i < 9:
-        if val >= int(tab[i][0]) and nom == False:
-            print(str(i + 1) + " - " + str(val) + "  -  " + "taper votre nom ...")
-            nom = True
-            i-=1
-        else:
-            if nom == True:
-                print(str(i + 2) + " - " + tab[i][0] + "  -  " + tab[i][1])
-            else:
+def afficheScore(tab, val):
+    i = 0
+    nom = False
+    if int(tab[9][0]) > val:
+        while i < len(tab):
+            if i < 10:
                 print(str(i + 1) + " - " + tab[i][0] + "  -  " + tab[i][1])
-        i+=1
+                i += 1
+            elif val >= int(tab[i][0]) and nom == False:
+                print("")
+                print(str(i + 1) + " - " + str(val) + "  -  " + "taper votre nom ...")
+                nom = True
+                i += 1
+            else:
+                i += 1
+                print(i)
+    else:
+        while i < 9:
+            if val >= int(tab[i][0]) and nom == False:
+                print(str(i + 1) + " - " + str(val) + "  -  " + "taper votre nom ...")
+                nom = True
+                i -= 1
+            else:
+                if nom == True:
+                    print(str(i + 2) + " - " + tab[i][0] + "  -  " + tab[i][1])
+                else:
+                    print(str(i + 1) + " - " + tab[i][0] + "  -  " + tab[i][1])
+            i += 1
 
 
+#tableau = triScore()
+#afficheScore(tableau, 2)
