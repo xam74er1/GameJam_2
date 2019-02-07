c2dd441bfaf8593843c4a482901c28b386204ab7
senetal <39625053+senetal@users.noreply.github.com>
2019-02-07 13:39:24 +0100

    Lvl 12 (mdr)

--- env_var.py
+++ env_var.py
@@ -1,8 +1,8 @@
 sprite_size = 16
-max_levels = 11
+max_levels = 12
 perso = "sprites/Blob/Blopchon.png"
 lvl_start = 0
-with_music = 1
+with_music = 0
 FPS = 22.0
 music_path = "music/Fly Octo Fly.ogg"
 wall_jump = 5
