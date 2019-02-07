8a2cf36ece12b765f065f2bb69ada417b8212624
xam74er <xam74er1@gmail.com>
2019-02-07 12:39:31 +0100

    Merge branch 'master' of https://github.com/xam74er1/GameJam_2
    # Conflicts:
    #	test_menu.py

--- env_var.py
+++ env_var.py
@@ -1,7 +1,7 @@
 sprite_size = 16
-max_levels = 10
+max_levels = 11
 perso = "sprites/Blob/Blopchon.png"
-lvl_start = 0
-with_music = 1
+lvl_start = 10
+with_music = 0
 FPS = 22.0
 music_path = "music/Fly Octo Fly.ogg"
